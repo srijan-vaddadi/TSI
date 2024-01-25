0c0ff31ee08adafcae8889236cc9c29a750dfdcd
Srijan <srijan.vaddadi@gmail.com>
2024-01-25 09:07:43 +0000

    commit

--- nextjs/src/app/[player]/[season]/page.tsx
+++ nextjs/src/app/[player]/[season]/page.tsx
@@ -1,35 +1,37 @@
 import Apicall from "@/app/apicall";
 import { table } from "console";
 
-interface PlayerPageProps{
-    params: {
-        player: string
-        season: string
-    }
+interface PlayerPageProps {
+  params: {
+    player: string;
+    season: string;
+  };
 }
 
 export default async function PlayerPage({ params }: PlayerPageProps) {
-    const player = await Apicall(params.player, params.season)
-    console.log(player)
+  const player = await Apicall(params.player, params.season);
+  console.log(player);
 
-    return(
-        <table>
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Age</th>
-                    <th>Photo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{player.firstname}</td>
-                    <td>{player.lastname}</td>
-                    <td>{player.age}</td>
-                    <td><img src={player.photo} alt="No image for player" /></td>
-                </tr>
-            </tbody>
-        </table>
-    )
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Age</th>
+          <th>Photo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{player.firstname}</td>
+          <td>{player.lastname}</td>
+          <td>{player.age}</td>
+          <td>
+            <img src={player.photo} alt="No image for player" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import Navbar from "@/components/navbar";
-import Grid from "@/components/grid";
 
 export default function root_page() {
-
   return (
     <main>
       <div>
         <h2>Football Player Deets</h2>
-        <h4>Enter in a player id then a season and see stats about your player</h4>
+        <h4>
+          Enter in a player id then a season and see stats about your player
+        </h4>
         <Navbar />
       </div>
     </main>
